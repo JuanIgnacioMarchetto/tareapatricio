bede8f612ca8795432cf4434287899229f6b9d45
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-20 19:10:07 -0300

    19:09-20/3/2023

--- javaclases.js
+++ javaclases.js
@@ -352,3 +352,36 @@ output(g[2].name);
 function output(msg) {
   out.innerHTML += msg + "<br/>";
 }*/
+/*var today = new Date();
+var day = today.getDay();
+var dayList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+document.getElementById("Day").innerHTML = 'Today is: ' + dayList[day] + '<br>';
+
+var hour = today.getHours();
+var minute = today.getMinutes();
+var second = today.getSeconds();
+var prepand = (hour >= 12)? " PM":" AM";
+hour = (hour > 12)? hour - 12:hour;
+
+if (hour === 12 && prepand === " PM") {
+  if (minute === 0 && second === 0) {
+    prepand = " Noon";
+  }
+  else {
+    hour = 12;
+    prepand = " PM";
+  }
+}
+
+if (hour === 12 && prepand === " AM") {
+  if (minute === 0 && second === 0) {
+    prepand = " Midnight";
+  }
+  else {
+    hour = 12;
+    prepand = " AM";
+  }
+}
+
+
+document.getElementById("Time").innerHTML = 'The time is: ' + hour + ':' + minute + ':' + second + prepand;*/
