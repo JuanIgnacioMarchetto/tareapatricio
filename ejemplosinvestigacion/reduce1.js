de92ac4c752a278b90d4d35857043850f205abf8
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-03-20 11:20:41 -0300

    11

---
+++ ejemplosinvestigacion/reduce1.js
@@ -0,0 +1,7 @@
+const numbers = 2
+
+/*const res =numbers.reduce((acc, item,index,arr)=>{
+return acc = acc+item;
+});
+console.log(res);*/
+console.log (numbers);
---
+++ javaclases.js
@@ -0,0 +1,84 @@
+/*console.log('estoy en el archivo javascript')
+
+/*
+todo esto no lo ejecuta el navegador
+esto es un comentario de varias lineas*/
+
+/*
+//variables
+
+let nombre;// declaracion de una variable
+
+nombre='pepe';
+
+console.log(nombre);
+
+ let apellido='pezer';
+
+ console.log(apellido);
+
+ var provincia='cordoba'; 
+
+ console.log=(provincia);
+
+ //alert ("hola mundo");
+
+ console.log=("mi nombre es : " + nombre + "y mi apellido es "
+ + apellido+"y vivo en la provincia de "+ provincia);
+
+*/
+/*
+let nombre ;
+
+nombre ="pepe";
+
+let apellido ;
+
+apellido= "perez";
+
+var provincia='cordoba';
+
+nombre="juan";
+
+apellido="paco",
+
+console.log=(nombre.apellido)
+console.log("mi nombre es : " + nombre + "y mi apellido es "
++ apellido+"y vivo en la provincia de "+ provincia)
+
+const dni=32195989;
+const iva=549855648;
+*/
+
+const iva= 8.21;
+const dni=322199565
+
+console.log("mi dni es :"+dni)
+let resultado=1000 + iva ;
+
+console.log(resultado);
+
+console.log("====================")
+
+console.log("====================")
+
+let falso= false;
+let verdadero=true;
+
+console.log (falso);
+console.log(verdadero);
+
+let uno= 25000;
+let dos=80000;
+
+resultado = uno + dos;
+console.log("el resultado de la suma es :"
++resultado);
+
+resultado = uno * dos;
+console.log("el resultado de la suma es :"
++resultado);
+
+resultado = uno % dos;
+console.log("el resultado de la suma es :"
++resultado);
